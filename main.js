50ca1630189ea601ae50c91723736011c4f65de2
Liuchangjin123 <2195469486@qq.com>
2022-08-31 17:45:34 +0800

    update

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: "数据加载中..."
+	})
+}
+
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
-		Home
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+			<swiper-item v-for="(item,i) in swiperList" :key="i">
+				<view class="swiper-item">
+					<image :src="item.image_src"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +15,41 @@
 	export default {
 		data() {
 			return {
-
+				// 轮播图数据列表
+				swiperList: []
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// console.log(res)
+				if (res.meta.status !== 200) {
+					return uni.showToast({
+						title: "数据请求失败",
+						duration: 1500,
+						icon: 'none'
+					})
+				}
+				this.swiperList = res.message
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%
+		}
+
+	}
 </style>
